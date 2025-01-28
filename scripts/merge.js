ee1d45a31cf61a67022247de9b05a3ad1d94cb40
Chlorine <git@chlor.me>
2025-01-28 19:59:21 +0800

    ci: use CommonJS

--- scripts/merge.js
+++ scripts/merge.js
@@ -1,16 +1,16 @@
-import { readdirSync, readFileSync, writeFileSync } from 'fs';
-import { join } from 'path';
+const fs = require('fs');
+const path = require('path');
 
 // Read all JSON files
-const jsonDir = join(process.cwd(), 'json');
-const files = readdirSync(jsonDir).filter(f => f.endsWith('.json'));
+const jsonDir = path.join(process.cwd(), 'json');
+const files = fs.readdirSync(jsonDir).filter(f => f.endsWith('.json'));
 
 // Create merged object
 const merged = {};
 files.forEach(file => {
-    const content = JSON.parse(readFileSync(join(jsonDir, file), 'utf8'));
+    const content = JSON.parse(fs.readFileSync(path.join(jsonDir, file), 'utf8'));
     Object.assign(merged, content);
 });
 
 // Write merged content
-writeFileSync('owo.json', JSON.stringify(merged, null, 2));
+fs.writeFileSync('owo.json', JSON.stringify(merged, null, 2));
